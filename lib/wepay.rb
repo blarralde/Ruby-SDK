b36a1e3176aaf5fff8332bbbff561c70bf30a947
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-05-30 11:58:21 -0700

    take into account _use_ssl for endpoints

--- lib/wepay.rb
+++ lib/wepay.rb
@@ -10,27 +10,28 @@ helps you make API calls to the WePay API v2
 =end
 
 class WePay
-	
-	STAGE_API_ENDPOINT = "https://stage.wepayapi.com/v2"
-	STAGE_UI_ENDPOINT = "https://stage.wepay.com/v2"
-	
-	PRODUCTION_API_ENDPOINT = "https://wepayapi.com/v2"
-	PRODUCTION_UI_ENDPOINT = "https://www.wepay.com/v2"
-		
+
+	STAGE_API_ENDPOINT = "://stage.wepayapi.com/v2"
+	STAGE_UI_ENDPOINT = "://stage.wepay.com/v2"
+
+	PRODUCTION_API_ENDPOINT = "://wepayapi.com/v2"
+	PRODUCTION_UI_ENDPOINT = "://www.wepay.com/v2"
+
 	# initializes the API application, api_endpoint should be something like 'https://stage.wepay.com/v2'
 	def initialize(_client_id, _client_secret, _use_stage = true, _use_ssl = true)
 		@client_id = _client_id
 		@client_secret = _client_secret
+    protocol = _use_ssl ? 'https' : 'http'
 		if _use_stage
-			@api_endpoint = STAGE_API_ENDPOINT
-			@ui_endpoint = STAGE_UI_ENDPOINT
+			@api_endpoint = protocol + STAGE_API_ENDPOINT
+			@ui_endpoint = protocol + STAGE_UI_ENDPOINT
 		else
-			@api_endpoint = PRODUCTION_API_ENDPOINT
-			@ui_endpoint = PRODUCTION_UI_ENDPOINT
+			@api_endpoint = protocol + PRODUCTION_API_ENDPOINT
+			@ui_endpoint = protocol + PRODUCTION_UI_ENDPOINT
 		end
 		@use_ssl = _use_ssl
 	end
-	
+
 	# make a call to the WePay API
 	def call(call, access_token = false, params = false)
 		# get the url
@@ -52,7 +53,7 @@ class WePay
 		# returns JSON response as ruby hash
 		JSON.parse(response.body)
 	end
-	
+
 	# this function returns the URL that you send the user to to authorize your API application
 	# the redirect_uri must be a full uri (ex https://www.wepay.com)
 	def oauth2_authorize_url(redirect_uri, user_email = false, user_name = false, permissions = "manage_accounts,view_balance,collect_payments,refund_payments,view_user")
@@ -60,10 +61,10 @@ class WePay
 		url += user_name ? '&user_name=' + CGI::escape(user_name) : ''
 		url += user_email ? '&user_email=' + CGI::escape(user_email) : ''
 	end
-	
+
 	#this function will make a call to the /v2/oauth2/token endpoint to exchange a code for an access_token
 	def oauth2_token(code, redirect_uri)
 		call('/oauth2/token', false, {'client_id' => @client_id, 'client_secret' => @client_secret, 'redirect_uri' => redirect_uri, 'code' => code })
 	end
-	
+
 end
